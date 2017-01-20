c4f670ed3594e6d7c402bb52bf99c9b800baa880
Unknown <mguzman@basecampcodingacademy.org>
2017-01-20 09:40:43 -0600

    added a registration form and forms.py

--- music/urls.py
+++ music/urls.py
@@ -4,15 +4,27 @@ from . import views
 app_name = 'music'
 
 urlpatterns = [
-    # /music/
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(),
+        name='index'),
+    url(r'^register/$',
+        views.UserFormView.as_view(),
+        name='register'),
+    url(r'^(?P<pk>[0-9]+)/$',
+        views.DetailView.as_view(),
+        name="detail"),
 
-    # /music/<album_id>/
-    url(r'^(?P<album_id>[0-9]+)/$',
-        views.detail, name="detail"),
+    # /music/album/add/
+    url(r'album/add/$',
+        views.AlbumCreate.as_view(),
+        name='album-add'),
 
-    # /music/<album_id>/favorite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$',
-        views.favorite,
-        name="favorite"),
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$',
+        views.AlbumUpdate.as_view(),
+        name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$',
+        views.AlbumDelete.as_view(),
+        name='album-delete'),
 ]
